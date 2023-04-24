536c05d29d10607202609edde14c2dbea0cd31a7
Suresh Surkheti <surkhetisuresh123@gmail.com>
2023-04-24 17:54:15 +0545

    Modified code api folder inside the watchlist_app of watchmate project also update in readme.md file

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-from watchlist_app.models import Movie
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from watchlist_app.models import Movie
+# from django.http import JsonResponse
 
 
-def movie_list(request):
-    movies = Movie.object.all()
-    data = {
-        "movies" : list(movies.values())
-        }
-    return JsonResponse(data)
+# def movie_list(request):
+#     movies = Movie.object.all()
+#     data = {
+#         "movies" : list(movies.values())
+#         }
+#     return JsonResponse(data)
    
