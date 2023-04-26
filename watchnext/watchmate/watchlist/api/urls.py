abfa8255ad87be956c5bfcd165200b4572222537
Suresh Surkheti <surkhetisuresh123@gmail.com>
2023-04-26 18:32:47 +0545

    added: status and class based views inside the api folder

--- watchnext/watchmate/watchlist/api/urls.py
+++ watchnext/watchmate/watchlist/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from watchlist.api.views import movie_list, movie_details
+# from watchlist.api.views import movie_list, movie_details
+from watchlist.api.views import MovieListAV, MovieDetailAV
 
 
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    path('<int:pk>', movie_details, name='movie-detail'),
+    # path('list/', movie_list, name='movie-list'),
+     path('list/', MovieListAV.as_view(), name='movie-list'),
+    # path('<int:pk>', movie_details, name='movie-detail'),
+     path('<int:pk>', MovieDetailAV.as_view(), name='movie-detail'),
 ]
--- watchnext/watchmate/watchlist/api/views.py
+++ watchnext/watchmate/watchlist/api/views.py
@@ -1,36 +1,21 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from watchlist.models import Movie
 from watchlist.api.serializers import MovieSerializer
 from .serializers import MovieSerializer
 
 
-@api_view(['GET','POST'])
-def movie_list(request):
-    
-    if request.method == 'GET':
+
+
+class MovieListAV(APIView):
+    def get(self, request):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data)
     
-    elif request.method == 'POST':
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-
-@api_view(['GET','PUT','DELETE'])
-def movie_details(request, pk):
-    
-    if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
-        serializer = MovieSerializer(movie)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
+    def post(self, request):
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -38,10 +23,75 @@ def movie_details(request, pk):
         else:
             return Response(serializer.errors)
         
-    if request.method == 'DELETE':
-        movie = Movie.objects.get(pk=pk)
+class MovieDetailAV(APIView):
+    def get(self, request, pk):
+        try:
+            movie = Movie.objects.get(pk =pk)
+        except Movie.DoesNotExist:
+            return Response({
+                'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    def put(self, request, pk):
+        movie = Movie.objects.get(pk = pk)
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk):
+        movie = Movie.objects.get(pk = pk)
         movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)  
+        
+            
+
+    
+    
+ #function based views
+# @api_view(['GET','POST'])
+# def movie_list(request):
+    
+#     if request.method == 'GET':
+#         movies = Movie.objects.all()
+#         serializer = MovieSerializer(movies, many=True)
+#         return Response(serializer.data)
+    
+#     elif request.method == 'POST':
+#         serializer = MovieSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+
+# @api_view(['GET','PUT','DELETE'])
+# def movie_details(request, pk):
+    
+#     if request.method == 'GET':
+#         try:
+#             movie = Movie.objects.get(pk=pk)
+#         except Movie.DoesNotExist:
+#             return Response({
+#                 'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+#         serializer = MovieSerializer(movie)
+#         return Response(serializer.data)
+
+#     elif request.method == 'PUT':
+#         serializer = MovieSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+#     if request.method == 'DELETE':
+#         movie = Movie.objects.get(pk=pk)
+#         movie.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
     
         
     
