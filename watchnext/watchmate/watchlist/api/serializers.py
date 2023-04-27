69287d353012a2146621bf41708bf064399dc6ee
Suresh Surkheti <surkhetisuresh123@gmail.com>
2023-04-27 10:55:02 +0545

    added: field validation, object validation and validatiors

--- watchnext/watchmate/watchlist/api/serializers.py
+++ watchnext/watchmate/watchlist/api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from watchlist.models import Movie
 
+
+#validators level validation
+def name_length(value):
+    if len(value)<2:
+        raise serializers.ValidationError("name is too short")
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
     
@@ -19,3 +25,17 @@ class MovieSerializer(serializers.Serializer):
         
     #instance carries old values and validated_data 
     #carries new values
+    
+    #object level validation
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and description should be different")
+        else:
+            return data
+    
+    #field level validation
+    # def validate_name(self, value):
+    #     if len(value)<2:
+    #         raise serializers.ValidationError("Name is too short!")
+    #     else:
+    #         return value
