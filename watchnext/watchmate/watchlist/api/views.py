4cf4a2563b082cab7d0e4820499fc4ccb17156be
Suresh Surkheti <surkhetisuresh123@gmail.com>
2023-05-01 13:48:17 +0545

    added : ModelViewset and ReadOnlyModelViewset

--- watchnext/watchmate/watchlist/api/views.py
+++ watchnext/watchmate/watchlist/api/views.py
@@ -54,26 +54,34 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     
 #     def post(self, request, *args, **kwargs):
 #         return self.create(request, *args, **kwargs)
+          
+# class StreamPlatformLM(viewsets.ModelViewSet):
+#     queryset = StreamPlatform.objects.all()
+#     serializer_class = StreamPlatformSerializer
+
+class StreamPlatformLM(viewsets.ReadOnlyModelViewSet):
+    queryset = StreamPlatform.objects.all()
+    serializer_class = StreamPlatformSerializer
            
-class StreamPlatformLM(viewsets.ViewSet):
+# class StreamPlatformLM(viewsets.ViewSet):
     
-    def list(self,request):
-      queryset = StreamPlatform.objects.all()
-      serializer = StreamPlatformSerializer(queryset, many=True)
-      return Response(serializer.data)
+#     def list(self,request):
+#       queryset = StreamPlatform.objects.all()
+#       serializer = StreamPlatformSerializer(queryset, many=True)
+#       return Response(serializer.data)
   
-    def retrieve(self,request, pk=None):
-        queryset = StreamPlatform.objects.all()
-        watchlist = get_object_or_404(queryset, pk=pk)
-        serializer = StreamPlatformSerializer(watchlist)
-        return Response(serializer.data)
-    def create(self, request):
-        serializer = StreamPlatformSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+#     def retrieve(self,request, pk=None):
+#         queryset = StreamPlatform.objects.all()
+#         watchlist = get_object_or_404(queryset, pk=pk)
+#         serializer = StreamPlatformSerializer(watchlist)
+#         return Response(serializer.data)
+#     def create(self, request):
+#         serializer = StreamPlatformSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
 
 
 class StreamPlatformAV(APIView):
