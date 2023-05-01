3165590a7470579ce4aa3603922cc2c5a351ac5d
Suresh Surkheti <surkhetisuresh123@gmail.com>
2023-05-01 13:38:46 +0545

    added: viewsets and routers

--- watchnext/watchmate/watchlist/api/urls.py
+++ watchnext/watchmate/watchlist/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 # from watchlist.api.views import movie_list, movie_details
-from watchlist.api.views import StreamPlatformAV, WatchListAV,WatchDetailAV,ReviewCreate, StreamPlatformDetailAV,ReviewList, ReviewDetail
+from watchlist.api.views import StreamPlatformAV,StreamPlatformLM, WatchListAV,WatchDetailAV,ReviewCreate, StreamPlatformDetailAV,ReviewList, ReviewDetail
+
+router = DefaultRouter()
+router.register('stream', StreamPlatformLM, basename='streamplatform')
+
 
 
 urlpatterns = [
@@ -8,8 +13,11 @@ urlpatterns = [
      path('list/', WatchListAV.as_view(), name='movie-list'),
     # # path('<int:pk>', movie_details, name='movie-detail'),
      path('<int:pk>', WatchDetailAV.as_view(), name='movie-detail'),
-     path('stream/', StreamPlatformAV.as_view(), name='stream'),
-     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
+    
+     path('', include(router.urls)),
+
+    #  path('stream/', StreamPlatformAV.as_view(), name='stream'),
+    #  path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
 
     #  path('review/',ReviewList.as_view(), name='review-list'),
     #  path('review/<int:pk>',ReviewDetail.as_view(), name='review-detail'),
--- watchnext/watchmate/watchlist/api/views.py
+++ watchnext/watchmate/watchlist/api/views.py
@@ -1,9 +1,11 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework import generics
 from rest_framework.response import Response
 # from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 # from rest_framework import mixins
+from  rest_framework import viewsets
 from watchlist.models import WatchList, StreamPlatform, Review
 
 from watchlist.api.serializers import WatchListSerializer, StreamPlatformSerializer, ReviewSerializer
@@ -52,7 +54,26 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     
 #     def post(self, request, *args, **kwargs):
 #         return self.create(request, *args, **kwargs)
-                      
+           
+class StreamPlatformLM(viewsets.ViewSet):
+    
+    def list(self,request):
+      queryset = StreamPlatform.objects.all()
+      serializer = StreamPlatformSerializer(queryset, many=True)
+      return Response(serializer.data)
+  
+    def retrieve(self,request, pk=None):
+        queryset = StreamPlatform.objects.all()
+        watchlist = get_object_or_404(queryset, pk=pk)
+        serializer = StreamPlatformSerializer(watchlist)
+        return Response(serializer.data)
+    def create(self, request):
+        serializer = StreamPlatformSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 
 class StreamPlatformAV(APIView):
