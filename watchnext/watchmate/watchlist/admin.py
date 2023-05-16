94b19d03e55d6d1ded21b75e9108df8a27b0ee81
Suresh Surkheti <surkhetisuresh123@gmail.com>
2023-05-16 18:12:33 +0545

    added: filtering, filter, search and ordering, and updating project

--- watchnext/watchmate/watchlist/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from watchlist.models import WatchList, StreamPlatform, Review
-
-# # Register your models here.
-admin.site.register(WatchList)
-admin.site.register(StreamPlatform)
-admin.site.register(Review)
--- watchnext/watchmate/watchlist/api/throttling.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.throttling import UserRateThrottle
-
-
-class ReviewCreateThrottle(UserRateThrottle):
-    scope = "review-create"
-
-
-class ReviewListThrottle(UserRateThrottle):
-    scope = "review-list"
